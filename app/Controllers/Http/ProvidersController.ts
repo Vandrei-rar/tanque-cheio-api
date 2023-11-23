473ef4a2283ac73f07d47cab975c2551b36f9584
Dener Cerantola <denercerantola3@hotmail.com>
2023-11-23 18:49:23 -0300

    Remoção de controllers desnecessários
    Removido controllers desnecessários e adiantado o controller de providers

--- app/Controllers/Http/ProvidersController.ts
+++ app/Controllers/Http/ProvidersController.ts
@@ -1,13 +1,32 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Provider from 'App/Models/Provider'
 
 export default class ProvidersController {
 
   public async index({}: HttpContextContract) {
-    return 'Index'
+    const provider = await Provider.all()
+    return provider
   }
 
-  public async store({}: HttpContextContract) {
-    return 'Store'
+  public async store({ request }: HttpContextContract) {
+    const name = request.input('name')
+    const description = request.input('description')
+    const logo = request.input('logo')
+    const rating = request.input('rating')
+    const localizationCoord = request.input('localizationCoord')
+    const localizationText = request.input('localizationText')
+    const status = request.input('status')
+
+    const provider = Provider.create({
+      name: name, description: description, logo: logo, rating: rating,
+       localizationCoord: localizationCoord, localizationText: localizationText,
+       status: status
+    })
+    if ((await provider).$isPersisted){
+      return 'Provider Cadastrado'
+    }else{
+      return 'Cadastro incorreto, tente novamente'
+    }
   }
 
   public async show({}: HttpContextContract) {
@@ -18,7 +37,11 @@ export default class ProvidersController {
     return 'Update'
   }
 
-  public async destroy({}: HttpContextContract) {
-    return 'Destroy'
+  public async destroy({ request }: HttpContextContract) {
+
+    const id = request.input('id')
+
+    await Provider.query().where('id', id).delete()
+    return 'Provider excluido'
   }
 }
